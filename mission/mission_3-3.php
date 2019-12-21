e098e650779326244afbffc76a4936464bd0aacd
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2019-12-21 17:11:04 +0900

    Deduplication

--- mission/mission_3-3.php
+++ mission/mission_3-3.php
@@ -7,7 +7,7 @@
 </head>
 
 <body>
-  <!--ここのmission_3-X-X.phpは必ず変えること！-->
+  <!--新規投稿フォーム / ここのmission_3-X-X.phpは必ず変えること！-->
   <form action="mission_3-3.php" method="post">
     <p>名前：<input type="text" name="personName" placeholder="名前"></p>
     <p>コメント：<input type="text" name="comment" placeholder="コメント"></p>
@@ -20,25 +20,17 @@
   </form>
   <?php
   $filename = "mission_3-3.txt";
-  // ファイルに書き込む
-  // 名前とコメントが入力されているか？
+  // 新規投稿
+  // 名前とコメントが入力されている？
   if (!(empty($_POST["personName"])) && !(empty($_POST["comment"]))) {
       $personName = $_POST["personName"];
       $comment = $_POST["comment"];
       // 現在の日時を取得
       date_default_timezone_set('Asia/Tokyo');
       $time = date('Y/m/d H:i:s');
-      // ファイルが存在するかをチェックする + ファイルが空かチェック
-      // 空かのチェックがないと削除して0になったとき$file_data[count($file_data)-1];でエラーが出てしまう
-      // https://www.flatflag.nir87.com/file_exists-888
+      // ファイルは存在するか？　＆　ファイルが空ではないか？
       if (file_exists($filename) && (!(empty(file($filename))))) {
-          /*
-          ファイルの行数をカウントする→これだと削除した後に追加すると同じ番号が現れてしまう...
-          https://so-zou.jp/web-app/tech/programming/php/file/lines.htm
-          $postNum = count(file($filename)) + 1;
-          */
-          // ファイルの最後の行を取得
-          // https://teratail.com/questions/3363
+          // 最後の行の番号+1
           $file_data = file($filename);
           $get_line = $file_data[count($file_data)-1];
           $text = explode("<>", $get_line);
@@ -50,24 +42,8 @@
       // ファイルに書き込み
       fwrite($fpa, $postNum."<>".$personName."<>".$comment."<>".$time."\n");
       fclose($fpa);
-      // ファイルを読み込み表示する
-      if (file_exists($filename)) {
-          $fpr = fopen($filename, 'r');
-          while (!feof($fpr)) {
-              $txt = fgets($fpr);
-              // explodeでテキストを分割する
-              // https://blog.codecamp.jp/php-explode
-              $text = explode("<>", $txt);
-              $printText = "";
-              foreach ($text as $value) {
-                  $printText .= $value." ";
-              }
-              echo "<p>".$printText."</p>";
-              //var_dump($text);
-          }
-          fclose($fpr);
-      }
-      // 削除フォームに入力されてるか？
+  // 削除番号
+  // 削除フォームに入力されてるか？
   } elseif (!(empty($_POST["deleteNumber"]))) {
       $deleteNumber = $_POST["deleteNumber"];
       $file_array = array();
@@ -86,29 +62,22 @@
               if ($text[0] != $deleteNumber) {
                   // 削除済みのものを書き込む
                   fwrite($fpw, $value);
-                  $printText = "";
-                  foreach ($text as $value) {
-                      $printText .= $value." ";
-                  }
-                  echo "<p>".$printText."</p>";
               }
           }
           fclose($fpw);
       }
-  } elseif (file_exists($filename)) {
-      // ↑がないと何も入力せずにボタンを押すと表示されていたものが消えてしまった
+  }
+  // ファイルを読ん込み表示する
+  if (file_exists($filename)) {
       $fpr = fopen($filename, 'r');
       while (!feof($fpr)) {
           $txt = fgets($fpr);
-          // explodeでテキストを分割する
-          // https://blog.codecamp.jp/php-explode
           $text = explode("<>", $txt);
           $printText = "";
           foreach ($text as $value) {
               $printText .= $value." ";
           }
           echo "<p>".$printText."</p>";
-          //var_dump($text);
       }
       fclose($fpr);
   }
