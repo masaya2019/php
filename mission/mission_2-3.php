4b3f25fe95300101900844b8015dce88d12d2af2
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2019-12-30 14:35:34 +0900

    Combine PHP processing

--- mission/mission_2-3.php
+++ mission/mission_2-3.php
@@ -1,17 +1,3 @@
-<?php
-if (!(empty($_POST["text"]))) {
-    $hensu = $_POST["text"];
-    $filename = "mission_2-3.txt";
-    // 追記に変更"w"=>"a"
-    $fp = fopen($filename, "a");
-    // 改行コードを入れる
-    // https://qiita.com/dayone80/items/00ab49e2d02be6a3bc87
-    fwrite($fp, $hensu."\n");
-    fclose($fp);
-    date_default_timezone_set('Asia/Tokyo');
-    $time = date('Y年m月d日 H時i分');
-}
-?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -26,30 +12,27 @@ if (!(empty($_POST["text"]))) {
     <input type="submit" value="送信">
   </form>
   <?php
+  // 投稿が空でないとき
   if (!(empty($_POST["text"]))) {
+      $hensu = $_POST["text"];
+      $filename = "mission_2-3.txt";
+      // ファイルに書き込む（追記）
+      // 追記に変更"w"=>"a"
+      $fp = fopen($filename, "a");
+      // 改行コードを入れる
+      // https://qiita.com/dayone80/items/00ab49e2d02be6a3bc87
+      fwrite($fp, $hensu."\n");
+      fclose($fp);
+      // 現在時刻の取得
+      date_default_timezone_set('Asia/Tokyo');
+      $time = date('Y年m月d日 H時i分');
+      // 受け取った投稿内容の表示
       if ($_POST["text"] == "完成！") {
           echo "おめでとう！";
       } else {
           echo "<p>".$time."に".$_POST["text"]."を受け付けました。</p>";
       }
   }
-/*
-＜問題点＞
-2-3で追加書き込みの反映がされない...
-
-＜解決策＞
-$filename = "mission_2-1.txt";
-<form action="mission_2-1.php" method="POST">
-↓
-$filename = "mission_2-3.txt";
-<form action="mission_2-3.php" method="POST">
-
-＜原因＞
-2-1をコピーして使っていたので
-$filename = "mission_2-1.txt";
-<form action="mission_2-1.php" method="POST">
-と2-1に飛ぶようになっていた
-*/
 ?>
 </body>
 
